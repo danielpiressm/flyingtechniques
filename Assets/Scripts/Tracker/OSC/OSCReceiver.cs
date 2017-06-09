d8cf17c99ccaa06b662b062b0521e523b6cc6bca
danielpiressm <danielpiressm@gmail.com>
2017-06-09 15:40:25 +0100

    OSC stuff for Wiimote

---
+++ Assets/Scripts/Tracker/OSC/OSCReceiver.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OSCReceiver : MonoBehaviour
+{
+
+	public string RemoteIP = "127.0.0.1";
+	public int SendToPort = 9000;
+	public int ListenerPort = 8000;
+
+	public float yaw1 = 0;
+	public float roll1 = 0;
+	public float pitch1 = 0;
+	public bool buttonB1 = false;
+
+	public float yaw2 = 0;
+	public float roll2 = 0;
+	public float pitch2 = 0;
+	public bool buttonB2 = false;
+
+	private Osc handler;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		UDPPacketIO udp = (UDPPacketIO)FindObjectOfType (typeof(UDPPacketIO));
+		udp.init (RemoteIP, SendToPort, ListenerPort);
+		handler = (Osc)FindObjectOfType (typeof(Osc));
+		handler.init (udp);
+		handler.SetAllMessageHandler (AllMessageHandler);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void AllMessageHandler (OscMessage oscMessage)
+	{
+		string msgString = Osc.OscMessageToString (oscMessage);
+		string msgAddress = oscMessage.Address;
+		//Debug.Log (msgString);
+
+		switch (msgAddress) {
+		case "/pie/rotation":
+			yaw1 = (float)oscMessage.Values [0];
+			roll1 = (float)oscMessage.Values [1];
+			pitch1 = (float)oscMessage.Values [2];
+			buttonB1 = (int)oscMessage.Values [3] == 1;
+			yaw2 = (float)oscMessage.Values [4];
+			roll2 = (float)oscMessage.Values [5];
+			pitch2 = (float)oscMessage.Values [6];
+			buttonB2 = (int)oscMessage.Values [7] == 1;
+			break;
+		}
+	}
+}
